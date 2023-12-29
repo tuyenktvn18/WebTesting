837b4de4ed19f61eb5d43da8e534b61d653b8144
tuyenktvn18 <thanhtuyenkt25@gmail.com>
2023-12-29 22:35:17 +0700

    delete unnecessary xpath in AddEmpUI class

--- src/main/java/com/practice/pageUI/pages/AddEmpUI.java
+++ src/main/java/com/practice/pageUI/pages/AddEmpUI.java
@@ -4,11 +4,8 @@ import org.openqa.selenium.By;
 
 public class AddEmpUI {
     public static final By FIRST_NAME_TEXT_BOX = By.xpath("//input[@name = 'firstName']");
-    public static final String FIRST_NAME_TEXT_BOX_STRING = "//input[@name = 'firstName']";
     public static final By MIDDLE_NAME_TEXT_BOX = By.xpath("//input[@name = 'middleName']");
-    public static final String MIDDLE_NAME_TEXT_BOX_STRING = "//input[@name = 'middleName']";
     public static final By LAST_NAME_TEXT_BOX = By.xpath("//input[@name = 'lastName']");
-    public static final String LAST_NAME_TEXT_BOX_STRING = "//input[@name = 'lastName']";
     public static final String EMPLOYEE_ID = "//label[text()='Employee Id']/..//following-sibling::div/input";
     public static final By SUCCESSFUL_SAVE_MESSAGE = By.xpath("//p[text()='Successfully Saved']");
 
